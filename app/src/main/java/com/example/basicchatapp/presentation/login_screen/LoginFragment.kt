c335d2bffb0f93fa66ff8c096869e8d7baed5e6c
kocerenes <eneskocerr41@gmail.com>
2022-06-16 23:49:19 +0300

    User sign up proccess is complete.

--- app/src/main/java/com/example/basicchatapp/presentation/login_screen/LoginFragment.kt
+++ app/src/main/java/com/example/basicchatapp/presentation/login_screen/LoginFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.basicchatapp.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class LoginFragment : Fragment() {
@@ -13,8 +18,12 @@ class LoginFragment : Fragment() {
     private var _binding : FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
 
     }
 
@@ -34,6 +43,14 @@ class LoginFragment : Fragment() {
         }
 
         binding.btnSignup.setOnClickListener{
+
+            auth.createUserWithEmailAndPassword(binding.ptEmail.text.toString(),binding.ptPassword.text.toString())
+                .addOnSuccessListener {
+                    val action = LoginFragmentDirections.actionLoginFragmentToChatFragment()
+                    findNavController().navigate(action)
+                }.addOnFailureListener{ exception ->
+                    Toast.makeText(requireContext(),exception.localizedMessage,Toast.LENGTH_LONG).show()
+                }
 
         }
 
